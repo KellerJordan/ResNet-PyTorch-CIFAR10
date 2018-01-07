36356ffa41dcc0b7ce8ff86499194a0b2109828b
Keller Jordan <kellerjordan1998@gmail.com>
2018-01-07 01:08:09 -0800

    implement ResBlock, ResNet class, training

--- main.py
+++ main.py
@@ -8,20 +8,22 @@ from torch.utils.data import DataLoader
 from torch.utils.data import sampler
 
 import torchvision.datasets as dset
+import torchvision.transforms as T
 
-from model import get_model
-# import torchvision.transforms as T
+from model import ResNet
 
-# import numpy as np
+import numpy as np
 
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--data-dir', default='./dataset', type=str,
                     help='path to dataset')
-parser.add_argument('--weight-decay', default=0.0, type=float,
+parser.add_argument('--weight-decay', default=0.0001, type=float,
                     help='parameter to decay weights')
 parser.add_argument('--num-epochs', default=10, type=int,
                     help='number of epochs to train for')
+parser.add_argument('--batch-size', default=128, type=int,
+                    help='size of each batch of cifar-10 training images')
 parser.add_argument('--print-every', default=100, type=int,
                     help='number of iterations to wait before printing')
 
@@ -33,52 +35,45 @@ def main(args):
     gpu_dtype = torch.cuda.FloatTensor
 
     # load model
-    model = get_model()
-    # model.cuda()
+    model = ResNet(5)
     model = model.type(gpu_dtype)
+    param_count = get_param_count(model)
+    print('Param count: %d' % param_count)
 
     criterion = nn.CrossEntropyLoss().cuda()
-    optimizer = optim.Adam(model.parameters(),
-                           weight_decay=args.weight_decay)
+    optimizer = optim.SGD(model.parameters(),
+                          lr=0.1, momentum=0.9, weight_decay=args.weight_decay)
 
     # get CIFAR-10 data
-    NUM_TRAIN = 49000
-    NUM_VAL = 1000
-    NUM_TEST = 10000
-    cifar10_train = dset.CIFAR10('./dataset', train=True, download=True, transform=T.ToTensor())
-    loader_train = DataLoader(cifar10_train, batch_size=64, sampler=ChunkSampler(NUM_TRAIN, 0))
-    # cifar10_val = dset.CIFAR10('./dataset', train=True, download=True, transform=T.ToTensor())
-    loader_val = DataLoader(cifar10_train, batch_size=64, sampler=ChunkSampler(NUM_TRAIN, NUM_VAL))
-    cifar10_test = dset.CIFAR10('./dataset', train=False, download=True, transform=T.ToTensor())
-    loader_test = DataLoader(cifar10_test, batch_size=64)
+    NUM_TRAIN = 45000
+    NUM_VAL = 5000
+
+    train_transform = T.Compose([
+        T.RandomHorizontalFlip(),
+        T.RandomCrop(32, padding=4),
+        T.ToTensor()])
+
+    cifar10_train = dset.CIFAR10('./dataset', train=True, download=True,
+                                 transform=train_transform)
+    loader_train = DataLoader(cifar10_train, batch_size=args.batch_size,
+                              sampler=ChunkSampler(NUM_TRAIN))
+    cifar10_val = dset.CIFAR10('./dataset', train=True, download=True,
+                               transform=T.ToTensor())
+    loader_val = DataLoader(cifar10_train, batch_size=args.batch_size,
+                            sampler=ChunkSampler(NUM_VAL, start=NUM_TRAIN))
+    cifar10_test = dset.CIFAR10('./dataset', train=False, download=True,
+                                transform=T.ToTensor())
+    loader_test = DataLoader(cifar10_test, batch_size=args.batch_size)
 
     # train model
     for epoch in range(args.num_epochs):
+        check_accuracy(model, loader_val)
         train(loader_train, model, criterion, optimizer, epoch)
-        
 
-def train(loader_train, model, criterion, optimizer, epoch):
-    print('Starting epoch %d / %d' % (epoch+1, args.num_epochs))
-    model.train()
-    for t, (X, y) in enumerate(loader_train):
-        X_var = Variable(X.type(gpu_dtype))
-        y_var = Variable(y.type(gpu_dtype))
-
-        scores = model(X_var)
-
-        loss = criterion(scores, y_var)
-        if (t+1) % args.print_every == 0:
-            print('t = %d, loss = %.4f' % (t+1, loss.data[0]))
-        
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
+    print('Final validation accuracy:')
+    check_accuracy(model, loader_val)
 
 def check_accuracy(model, loader):
-    if loader.dataset.train:
-        print('Checking accuracy on validation set')
-    else:
-        print('Checking accuracy on test set')   
     num_correct = 0
     num_samples = 0
     model.eval()
@@ -93,6 +88,27 @@ def check_accuracy(model, loader):
 
     acc = float(num_correct) / num_samples
     print('Got %d / %d correct (%.2f)' % (num_correct, num_samples, 100 * acc))
+
+def train(loader_train, model, criterion, optimizer, epoch):
+    print('Starting epoch %d / %d' % (epoch+1, args.num_epochs))
+    model.train()
+    for t, (X, y) in enumerate(loader_train):
+        X_var = Variable(X.type(gpu_dtype))
+        y_var = Variable(y.type(gpu_dtype))
+
+        scores = model(X_var)
+
+        loss = criterion(scores, y_var)
+        if (t+1) % args.print_every == 0:
+            print('t = %d, loss = %.4f' % (t+1, loss.data[0]))
+
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+def get_param_count(model):
+    param_counts = [np.prod(p.size()) for p in model.parameters()]
+    return sum(param_counts)
 
 class ChunkSampler(sampler.Sampler):
     def __init__(self, num_samples, start=0):
--- model.py
+++ model.py
@@ -1,27 +1,67 @@
-import torch
 import torch.nn as nn
-from torch.autograd import Variable
-
-import torchvision.transforms as T
-
-# import numpy as np
 
 
-def get_model():
-    model_base = nn.Sequential(
-        nn.Conv2d(3, 32, kernel_size=7, stride=1),
-        nn.ReLU(inplace=True),
-        nn.BatchNorm2d(32),
-        nn.MaxPool2d(2, stride=2),
-        Flatten(),
-        nn.Linear(5408, 1024),
-        nn.ReLU(inplace=True),
-        nn.Linear(1024, 10),
-    )
-
-    return model_base
-
-class Flatten(nn.Module):
+class ResNet(nn.Module):
+    def __init__(self, n=7):
+        super(ResNet, self).__init__()
+        self.conv1 = nn.Conv2d(3, 16, kernel_size=3, stride=1, padding=1)
+        self.norm1 = nn.BatchNorm2d(16)
+        self.relu1 = nn.ReLU(inplace=True)
+        self.layers1 = self._make_layer(n, 16, 3, 1)
+        self.layers2 = self._make_layer(n, 32, 16, 2)
+        self.layers3 = self._make_layer(n, 64, 32, 2)
+        self.avgpool = nn.AvgPool2d(8)
+        self.linear = nn.Linear(64, 8)
+    
+    def _make_layer(self, layer_count, channels, channels_in, stride):
+        return nn.Sequential(
+            ResBlock(channels, channels_in),
+            *[ResBlock(channels) for _ in range(layer_count-1)])
+    
     def forward(self, x):
-        N, C, H, W = x.size()
-        return x.view(N, -1)
+        out = self.conv1(x)
+        out = self.norm1(x)
+        out = self.relu1(x)
+        out = self.layers1(x)
+        out = self.layers2(x)
+        out = self.layers3(x)
+        out = self.avgpool(x)
+        out = self.linear(x)
+        return out
+
+
+class ResBlock(nn.Module):
+    def __init__(self, num_filters, channels_in=None, stride=1, dropout=True):
+        super(ResBlock, self).__init__()
+        
+        # uses 1x1 convolutions for downsampling
+        if not channels_in:
+            channels_in = num_filters
+            self.projection = None
+        else:
+            self.projection = nn.Conv2d(channels_in, num_filters, kernel_size=1, stride=stride)
+        self.dropout = dropout
+
+        self.conv1 = nn.Conv2d(channels_in, num_filters, kernel_size=3, stride=stride, padding=1)
+        self.bn1 = nn.BatchNorm2d(num_filters)
+        self.relu1 = nn.ReLU(inplace=True)
+        self.conv2 = nn.Conv2d(num_filters, num_filters, kernel_size=3, stride=1, padding=1)
+        self.bn2 = nn.BatchNorm2d(num_filters)
+        if self.dropout:
+            self.dropout = nn.Dropout(inplace=True)
+        self.relu2 = nn.ReLU(inplace=True)
+
+    def forward(self, x):
+        residual = x
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu1(out)
+        out = self.conv2(out)
+        out = self.bn2(out)
+        if self.dropout:
+            out = self.dropout(out)
+        if self.projection:
+            residual = self.projection(x)
+        out += residual
+        out = self.relu2(out)
+        return out
